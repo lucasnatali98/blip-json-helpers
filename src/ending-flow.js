65d3f41eb7934a31826e154f4e1d89e374b8ab68
Lucas Natali <lucasnatali16@gmail.com>
2024-06-07 11:22:00 -0300

    feat: ending-flow.js v.0.0

--- src/ending-flow.js
+++ src/ending-flow.js
@@ -1,12 +1,66 @@
+import { arraysDeepEqual, generateUUID } from "./utils/util";
+
+const endingFlowConditionOutput = (info) => {
+  const stateId = info.stateId;
+
+  return {
+    stateId: stateId,
+    $id: generateUUID(),
+    conditions: [
+      {
+        source: "input",
+        comparison: "equals",
+        values: ["sair", "encerrar"],
+      },
+    ],
+  };
+};
 export function endingFlow(flow, payload) {
   try {
     const { endingBlockInfo, endingCondition } = payload;
+    const endingBlockId = endingBlockInfo.stateId;
     const blockToPassArray = ["welcome", "desk"];
+    const resultFlow = {};
     for (const [key, value] of Object.entries(flow)) {
       if (blockToPassArray.includes(key)) {
         continue;
       }
+
+      const contentActions = value.$contentActions ?? null;
+
+      if (contentActions === null) {
+        continue;
+      }
+
+      for (const contentAction of contentActions) {
+        if (!contentAction.input?.bypass) {
+          const endingFlowOutputCondition = endingFlowConditionOutput({
+            stateId: key,
+          });
+
+          const isEndingFlowOutputConditionAlreadyExists =
+            contentAction.$conditionOutputs.some(
+              (conditionOutput) =>
+                conditionOutput.stateId === endingBlockId ||
+                arraysDeepEqual(
+                  conditionOutput.conditions,
+                  endingFlowOutputCondition.conditions
+                )
+            );
+
+          if (!isEndingFlowOutputConditionAlreadyExists) {
+            contentAction.$conditionOutputs.splice(
+              0,
+              0,
+              endingFlowOutputCondition
+            );
+          }
+        }
+      }
     }
+
+    resultFlow = { ...flow };
+    return resultFlow;
   } catch (error) {
     return error;
   }
--- src/utils/util.js
+++ src/utils/util.js
@@ -3,3 +3,46 @@ import crypto from "node:crypto";
 export function generateUUID() {
   return crypto.randomUUID();
 }
+export function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (
+    obj1 == null ||
+    obj2 == null ||
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object"
+  ) {
+    return false;
+  }
+
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function arraysDeepEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!deepEqual(arr1[i], arr2[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
