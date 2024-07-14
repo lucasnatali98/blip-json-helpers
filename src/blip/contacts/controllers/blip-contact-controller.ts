07385d6c4c35e6f80b3b57b35ae36f713b1ef686
Lucas Natali <lucasnatali16@gmail.com>
2024-07-14 15:54:22 -0300

    feat: try catch + respostas http basicas para controller do blip contact

--- src/blip/contacts/controllers/blip-contact-controller.ts
+++ src/blip/contacts/controllers/blip-contact-controller.ts
@@ -5,6 +5,8 @@ import {
   BlipHttpResponseTemplateWithoutResource,
 } from "@/blip/blip-response-builder";
 import { Contact } from "../contact";
+import { HttpDataResponseBuilder } from "@/shared/http-data-response-builder";
+import { InternalError } from "@/shared/errors";
 
 export interface BlipContactControler {
   getContacts(): Promise<HttpDataResponse<BlipHttpResponseTemplate<Contact[]>>>;
@@ -22,21 +24,61 @@ export class BlipContactControllerImpl implements BlipContactControler {
   async getContacts(): Promise<
     HttpDataResponse<BlipHttpResponseTemplate<Contact[]>>
   > {
-    throw new Error("Method not implemented.");
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplate<Contact[]>>()
+        .create()
+        .withOkMessage({} as BlipHttpResponseTemplate<Contact[]>)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplate<Contact[]>>()
+        .create()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
   }
   async add(
     contact: Contact
   ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>> {
-    throw new Error("Method not implemented.");
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .create()
+        .withOkMessage({} as BlipHttpResponseTemplateWithoutResource)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .create()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
   }
   async update(
     contact: Contact
   ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>> {
-    throw new Error("Method not implemented.");
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .create()
+        .withOkMessage({} as BlipHttpResponseTemplateWithoutResource)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .create()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
   }
   async get(
     identity: string
   ): Promise<HttpDataResponse<BlipHttpResponseTemplate<Contact>>> {
-    throw new Error("Method not implemented.");
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplate<Contact>>()
+        .create()
+        .withOkMessage({} as BlipHttpResponseTemplate<Contact>)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplate<Contact>>()
+        .create()
+        .withInfoErrorMessage([InternalError])
+        .build();
+    }
   }
 }
