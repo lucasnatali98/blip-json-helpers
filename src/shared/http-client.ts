57a706f6877e4276e1ab798cf2c5b59952fdd8e5
Lucas Natali <lucasnatali16@gmail.com>
2024-07-14 16:18:07 -0300

    chore: implementando os metodos do http client axios

--- src/shared/http-client.ts
+++ src/shared/http-client.ts
@@ -1,40 +1,56 @@
-import axios from "axios"
+import axios from "axios";
 export interface HttpClient {
-    get<T>(url: string, config?: any): Promise<T>
-    post<T>(url: string, data: any, config?: any): Promise<T>;
-    put<T>(url: string, data: any, config?: any): Promise<T>;
-    delete<T>(url: string, config?: any): Promise<T>;
-
+  get<T>(url: string, config?: any): Promise<T>;
+  post<T>(url: string, data: any, config?: any): Promise<T>;
+  put<T>(url: string, data: any, config?: any): Promise<T>;
+  delete<T>(url: string, config?: any): Promise<T>;
 }
 
 export class AxiosHttpClient implements HttpClient {
-    async post<T>(url: string, data: any, config?: any): Promise<T> {
-        throw new Error("Method not implemented.");
-    }
-    async put<T>(url: string, data: any, config?: any): Promise<T> {
-        throw new Error("Method not implemented.");
-    }
-    async delete<T>(url: string, config?: any): Promise<T> {
-        throw new Error("Method not implemented.");
-    }
-    async get<T>(url: string, config?: any): Promise<T> {
-        return await axios.get(url, config)
-    }
+  async post<T>(url: string, data: any, config?: any): Promise<T> {
+    const response = await axios.post(url, data, config);
+    return {
+      status: response.status,
+      ...response.data,
+    };
+  }
+  async put<T>(url: string, input: any, config?: any): Promise<T> {
+    const response = await axios.put(url, input, config);
+    const data = response.data;
+    return {
+      status: response.status,
+      ...data,
+    };
+  }
+  async delete<T>(url: string, config?: any): Promise<T> {
+    const response = await axios.delete(url, config);
+    const data = response.data;
+    return {
+      status: response.status,
+      ...data,
+    };
+  }
+  async get<T>(url: string, config?: any): Promise<T> {
+    const response = await axios.get(url, config);
+    const data = response.data;
+    return {
+      status: response.status,
+      ...data,
+    };
+  }
 }
 
 export class HttpClientProvider {
-    private static instance: HttpClient
-    static getHttpClient(): HttpClient {
-        const config = {
-            httpClientTool: "axios"
-        }
-        if(!this.instance) {
-            
-            if(config.httpClientTool === "axios") {
-                this.instance = new AxiosHttpClient()
-            }
-            
-        }
-        return this.instance
+  private static instance: HttpClient;
+  static getHttpClient(): HttpClient {
+    const config = {
+      httpClientTool: "axios",
+    };
+    if (!this.instance) {
+      if (config.httpClientTool === "axios") {
+        this.instance = new AxiosHttpClient();
+      }
     }
+    return this.instance;
+  }
 }
