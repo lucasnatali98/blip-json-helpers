3383fe957d88c0dce66eb3299898b3a6a6c45231
Lucas Natali <lucasnatali16@gmail.com>
2024-07-14 16:07:36 -0300

    feat: esqueleto inicial para controller de event tracking da blip

--- src/blip/event-tracking/controllers/blip-event-tracking-controller.ts
+++ src/blip/event-tracking/controllers/blip-event-tracking-controller.ts
@@ -1,12 +1,142 @@
+import {
+  BlipHttpResponseTemplateWithoutResource,
+  BlipHttpResponseTemplate,
+} from "@/blip/blip-response-builder";
 import { HttpDataResponse } from "@/shared/http-data-response";
-import {} from "";
+import {
+  CreateEventRequestDto,
+  GetEventDetailsRequestDto,
+  GetEventDetailsResponse,
+  GetEventCategoriesResponse,
+  GetEventCountersResponse,
+  CreateEventWithContactRequestDto,
+} from "../infrastructure/data/blip-event-tracking-dto";
+import { HttpDataResponseBuilder } from "@/shared/http-data-response-builder";
+import { InternalError } from "@/shared/errors";
+
 export interface BlipEventTrackingController {
-  getEventDetails(): Promise<HttpDataResponse<any>>;
-  createEvent(): Promise<HttpDataResponse<any>>;
-  getEventCategories(): Promise<HttpDataResponse<any>>;
-  getEventCounters(): Promise<HttpDataResponse<any>>;
-  deleteEventCategory(): Promise<HttpDataResponse<any>>;
+  createEvent(
+    dto: CreateEventRequestDto
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>>;
+  getEventDetails(
+    dto: GetEventDetailsRequestDto
+  ): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventDetailsResponse>>
+  >;
+  getEventCategories(): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventCategoriesResponse>>
+  >;
+  getEventCounters(
+    uri: string
+  ): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventCountersResponse>>
+  >;
+  createEventWithContact(
+    dto: CreateEventWithContactRequestDto
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>>;
+  deleteEventCategory(
+    uri: string
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>>;
 }
 
 export class BlipEventTrackingControllerImpl
-  implements BlipEventTrackingController {}
+  implements BlipEventTrackingController
+{
+  async createEvent(
+    dto: CreateEventRequestDto
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>> {
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withOkMessage({} as BlipHttpResponseTemplateWithoutResource)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+
+  async getEventDetails(
+    dto: GetEventDetailsRequestDto
+  ): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventDetailsResponse>>
+  > {
+    try {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventDetailsResponse>
+      >()
+        .withOkMessage({} as BlipHttpResponseTemplate<GetEventDetailsResponse>)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventDetailsResponse>
+      >()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+  async getEventCategories(): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventCategoriesResponse>>
+  > {
+    try {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventCategoriesResponse>
+      >()
+        .withOkMessage(
+          {} as BlipHttpResponseTemplate<GetEventCategoriesResponse>
+        )
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventCategoriesResponse>
+      >()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+  async getEventCounters(
+    uri: string
+  ): Promise<
+    HttpDataResponse<BlipHttpResponseTemplate<GetEventCountersResponse>>
+  > {
+    try {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventCountersResponse>
+      >()
+        .withOkMessage({} as BlipHttpResponseTemplate<GetEventCountersResponse>)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<
+        BlipHttpResponseTemplate<GetEventCountersResponse>
+      >()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+  async createEventWithContact(
+    dto: CreateEventWithContactRequestDto
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>> {
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withOkMessage({} as BlipHttpResponseTemplateWithoutResource)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+  async deleteEventCategory(
+    uri: string
+  ): Promise<HttpDataResponse<BlipHttpResponseTemplateWithoutResource>> {
+    try {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withOkMessage({} as BlipHttpResponseTemplateWithoutResource)
+        .build();
+    } catch (error: any) {
+      return new HttpDataResponseBuilder<BlipHttpResponseTemplateWithoutResource>()
+        .withInternalErrorMessage([InternalError])
+        .build();
+    }
+  }
+}
